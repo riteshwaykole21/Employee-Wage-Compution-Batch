0fcd90f39ba823e990741bf06fdcf09ac9ed7715
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-03 12:59:45 +0530

    [Ritesh]add : Add Part Time Employee Wage

--- EmployeeWageCalculation/CheckAttendances.cs
+++ EmployeeWageCalculation/CheckAttendances.cs
@@ -11,15 +11,21 @@ namespace EmployeeWageCalculation
         public void Attendances()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HRS = 20;
             int empHrs = 0;
             int empWages = 0;
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if (empcheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee Is Present");
+                Console.WriteLine("Employee Is Present Full Time");
                 empHrs = 8;
+            }
+            else if (empcheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee Is Present Part Time ");
+                empHrs = 4;
             }
             else
             {
