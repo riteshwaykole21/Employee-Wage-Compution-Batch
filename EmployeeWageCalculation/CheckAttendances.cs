43a02d2f61739c8e9fa79a1d1a2215b8f42b0c48
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-03 12:02:16 +0530

    [Ritesh]add : Check Employee Present Or Absent

---
+++ EmployeeWageCalculation/CheckAttendances.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+    public class CheckAttendances
+    {
+        public void Attendances()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is a Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }       
+}
---
+++ EmployeeWageCalculation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome In Employee Compution ");
+            CheckAttendances EmpCheck = new CheckAttendances();
+            EmpCheck.Attendances();
+            Console.ReadLine();
+        }
+    }
+}
