b79ad236af31d0fed0e9023d2cb178553902320d
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-04 20:44:07 +0530

    [Ritesh]add : using Switch Case in Employee Wage

--- EmployeeWageCalculation/CheckAttendances.cs
+++ EmployeeWageCalculation/CheckAttendances.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,27 +11,27 @@ namespace EmployeeWageCalculation
     {
         public void Attendances()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HRS = 20;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int EMP_RATE_PER_HRS = 20;
             int empHrs = 0;
             int empWages = 0;
             Random random = new Random();
             int empcheck = random.Next(0, 3);
-            if (empcheck == IS_FULL_TIME)
+            switch (empcheck)
             {
-                Console.WriteLine("Employee Is Present Full Time");
-                empHrs = 8;
-            }
-            else if (empcheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee Is Present Part Time ");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee Is Present Full Time");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee Is Present Part Time ");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
             }
             empWages = EMP_RATE_PER_HRS * empHrs;
             Console.WriteLine("empWages = " + empWages);
